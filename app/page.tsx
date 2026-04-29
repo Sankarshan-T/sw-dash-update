900ad5de8b8b49dd3b14443e528b383e85cc10ff
CoolCream <mahasankarshant@gmail.com>
2026-04-30 05:03:29 +0530

    next app

---
+++ app/page.tsx
@@ -0,0 +1,25 @@
+import { Button } from "@/components/ui/button";
+import Image from "next/image";
+import Link from "next/link";
+
+export default function Home() {
+  return (
+    <div className="flex flex-col flex-1 items-center justify-center w-screen h-screen space-y-4">
+      <Image
+        src={'/sw.png'}
+        alt="SW logo"
+        width={200}
+        height={200}
+      />
+      <h1 className="text-4xl font-mono font-bold">SW-DASH</h1>
+      <p>
+        The private system for the <strong className="font-mono">Shipwrights</strong>
+      </p>
+      <Link href={'/admin'}>
+        <Button className="px-24 py-5 text-xl">
+          Enter
+        </Button>
+      </Link>
+    </div>
+  );
+}
